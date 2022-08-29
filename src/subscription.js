a5cfbece11cc6b853a09d7d1a01639b04607e73b
Dmirty <dezimbo84@gmail.com>
2022-08-29 18:53:19 +0300

    fix

--- src/subscription.js
+++ src/subscription.js
@@ -1,17 +1,17 @@
 const convertedVapidKey = urlBase64ToUint8Array(process.env.REACT_APP_PUBLIC_VAPID_KEY)
 
 function urlBase64ToUint8Array(base64String) {
-    const padding = "=".repeat((4 - base64String.length % 4) % 4)
-    // eslint-disable-next-line
-    const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/")
+  var padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+  // eslint-disable-next-line no-useless-escape
+  var base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
 
-    const rawData = window.atob(base64)
-    const outputArray = new Uint8Array(rawData.length)
+  var rawData = window.atob(base64)
+  var outputArray = new Uint8Array(rawData.length)
 
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i)
-    }
-    return outputArray
+  for (var i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
 }
 
 function sendSubscription(subscription) {
